5d01fbb0d3661fd70e5733c8d0d6d8b0ba84dbf3
David Andrea <davejandrea@gmail.com>
2024-05-26 19:50:55 -0300

    [red] mom returns true

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,19 @@
 
 describe('palindrome checker', () => {
 
+    test("'mom' should return true", () => {
+        expect(isPalindrome("mom")).toBe(true);
+    })
+
+    // "mom" returns true
+    // "Mom" returns true
+    // "MoM" returns true
+    // "Momx" returns false
+    // "xMomx" returns true
+    // "Was It A Rat I Saw" returns true
+    // "Never Odd or Even" returns true
+    // "Never Odd or Even1" returns false
+    // "1Never Odd or Even1" returns true
+
+
 })
