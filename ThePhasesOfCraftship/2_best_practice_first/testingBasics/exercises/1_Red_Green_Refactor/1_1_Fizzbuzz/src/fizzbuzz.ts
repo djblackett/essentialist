292dd021e8537b548ffba221afaf5c0183bef5de
David Andrea <davejandrea@gmail.com>
2024-05-26 15:51:50 -0300

    [refactor] 3 and 6 should return fizz - passing

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,7 +2,7 @@
 export function fizzbuzz(num: number) {
     if (num % 5 === 0) {
         return "buzz"
-    } else if (num === 3 || num == 6) {
+    } else if (num % 3 === 0) {
         return "fizz"
     }
     return num.toString(10)
