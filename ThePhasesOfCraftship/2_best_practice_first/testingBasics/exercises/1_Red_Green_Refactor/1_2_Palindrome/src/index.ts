2b617ad843b96406d20358fa9ff4df45fce7ee82
David Andrea <davejandrea@gmail.com>
2024-05-26 20:15:08 -0300

    [green] 'Was It A Rat I Saw' returns true

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,4 +1,11 @@
 
 export function isPalindrome(text: string) {
-    return text.toLowerCase().split("").reverse().join("") === text.toLowerCase();
+    return text
+        .replaceAll(/ +/g, "")
+        .toLowerCase()
+        .split("")
+        .reverse()
+        .join("") === text
+        .toLowerCase()
+        .replaceAll(/ +/g, "");
 }
