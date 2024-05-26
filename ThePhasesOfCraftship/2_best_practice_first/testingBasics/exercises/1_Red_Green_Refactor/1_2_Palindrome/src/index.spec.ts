7d58afdd39f6e80ec0dec5943cc5fdc859c6a416
David Andrea <davejandrea@gmail.com>
2024-05-26 20:39:17 -0300

    [green] removed comments containing testing requirements

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -18,15 +18,4 @@ describe('palindrome checker', () => {
         }
     );
 
-    // "mom" returns true
-    // "Mom" returns true
-    // "MoM" returns true
-    // "Momx" returns false
-    // "xMomx" returns true
-    // "Was It A Rat I Saw" returns true
-    // "Never Odd or Even" returns true
-    // "Never Odd or Even1" returns false
-    // "1Never Odd or Even1" returns true
-
-
 })
