88f99c661fb29fdb6ca867adda0b517fb38bfdb9
David Andrea <davejandrea@gmail.com>
2024-05-26 15:40:42 -0300

    add test for input of 5 return buzz - failing

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,7 +4,14 @@ describe("fizzbuzz", () => {
 
     test("should return a string", () => {
         expect(typeof fizzbuzz(5)).toBe("string")
-    })
+    });
+
+    test("input of 5 should return buzz", () => {
+        expect(fizzbuzz(5)).toEqual("buzz");
+    });
+
+
+
 });
 
 
