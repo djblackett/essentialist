5514c748a566466e6d10cdc4412b4bfee3f5790e
David Andrea <davejandrea@gmail.com>
2024-05-26 18:49:30 -0300

    [new] added test.each() to simplify similar test cases

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -2,9 +2,27 @@ import {fizzbuzz} from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
 
+    const inputs = [3, 5, 9, 10, 15, 42, 43, 45]
+    const results = ["fizz", "buzz", "fizz", "buzz", "fizzbuzz", "fizz", "43", "fizzbuzz"]
+
+    const testCases = inputs.map((input, index) => [input, results[index]]);
+
+    test.each(testCases)(
+        "given %p as input, returns %p",
+        (input, expectedResult) => {
+            const result = fizzbuzz(input as number);
+            expect(result).toEqual(expectedResult);
+        }
+    );
+
+
     test("should return a string", () => {
         expect(typeof fizzbuzz(5)).toBe("string")
     });
+
+
+
+    test.each
 
     test("input of 5 should return buzz", () => {
         expect(fizzbuzz(5)).toEqual("buzz");
