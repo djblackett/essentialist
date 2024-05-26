af79dbc585bf00c659dab811a75019443981e8b1
David Andrea <davejandrea@gmail.com>
2024-05-26 18:51:17 -0300

    [new] removed redundant test cases

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -15,34 +15,9 @@ describe("fizzbuzz", () => {
         }
     );
 
-
     test("should return a string", () => {
         expect(typeof fizzbuzz(5)).toBe("string")
     });
-
-
-
-    test.each
-
-    test("input of 5 should return buzz", () => {
-        expect(fizzbuzz(5)).toEqual("buzz");
-    });
-
-    test("10 should return buzz", () => {
-        expect(fizzbuzz(10)).toEqual("buzz");
-        });
-
-    test("3 should return fizz", () => {
-        expect(fizzbuzz(3)).toEqual("fizz");
-    })
-
-    test("9 should return fizz", () => {
-        expect(fizzbuzz(9)).toEqual("fizz");
-    })
-
-    test("15 should return fizzbuzz", () => {
-        expect(fizzbuzz(15)).toEqual("fizzbuzz")
-    })
 
     test("102 should throw an error", () => {
         expect(() => fizzbuzz(102)).toThrow(new Error("Numbers must be between 1 and 100 (inclusive)"))
@@ -50,18 +25,6 @@ describe("fizzbuzz", () => {
 
     test("non-number inputs should throw error", () => {
         expect(() => fizzbuzz("25" as unknown as number)).toThrow("Input must be a number")
-    })
-
-    test("43 should return '43'", () => {
-        expect(fizzbuzz(43)).toEqual("43");
-    });
-
-    test("42 should return fizz", () => {
-        expect(fizzbuzz(42)).toEqual("fizz");
-    });
-
-    test("45 should return fizzbuzz", () => {
-        expect(fizzbuzz(45)).toEqual("fizzbuzz");
     })
 });
 
